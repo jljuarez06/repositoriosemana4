fd1911b1ce1d0b4028dd0c67aac197308dae6faf
jljuarez <jljuarez@uamv.edu.ni>
2026-09-07 10:30:18 -0600

    agregar ejercicio2

--- nombre_empresa.py
+++ nombre_empresa.py
@@ -5,3 +5,14 @@ def mostrar_empresa():
     print(nombre_empresa)
 
 mostrar_empresa()
+
+#ejercicio2
+def calcular_total():
+    total = 100
+    print("dentro de la funcion:", total)
+
+calcular_total()
+
+print("fuera de la funcion:", total)
+
+#el error es porque total es una variable local: solamente existe dentro de calcular_total().
